d6ba353c7e45d142eab7393c84733f43d835ee89
Peter Benjamin <petermbenjamin@gmail.com>
2018-08-05 15:17:09 -0700

    Fix script

--- heptio-sonobuoy/step1.sh
+++ heptio-sonobuoy/step1.sh
@@ -1,7 +1,7 @@
 echo "Launching Kubernetes cluster..."
 launch.sh
 echo "Downloading Go installer..."
-curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-chmod +x ~/bin/gimme
+curl -sL -o ~/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ~/gimme
 echo "Installing Go..."
-gimme stable
+~/gimme stable
