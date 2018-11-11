482fe06d3e082e88a98ed3b614189de1eb18e93b
Peter Benjamin <petermbenjamin@gmail.com>
2018-11-11 09:48:21 -0800

    Run setup script on intro

---
+++ heptio-sonobuoy/setup.sh
@@ -0,0 +1,5 @@
+launch.sh
+sudo curl -sLo /usr/local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+sudo chmod +x /usr/local/bin/gimme
+gimme stable
+. ~/.gimme/envs/latest.env
--- heptio-sonobuoy/step1.sh
+++
@@ -1,5 +0,0 @@
-launch.sh
-curl -sL -o ~/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-chmod +x ~/gimme
-~/gimme stable
-source ~/.gimme/envs/latest.env
