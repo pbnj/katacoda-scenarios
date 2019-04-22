80a3c81817575b653c702d3498daac60968c4a63
Peter Benjamin <petermbenjamin@gmail.com>
2019-04-22 00:07:56 -0700

    Fix setup script

--- kind/setup.sh
+++ kind/setup.sh
@@ -1,15 +1,15 @@
 # install deps# 
 ## go (via https://github.com/travis-ci/gimme)
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=stable bash)" \
-	&& export PATH=$PATH:$HOME/go/bin
+&& export PATH=$PATH:$HOME/go/bin
 
 ## docker should already be installed on katacoda ubuntu image
 docker version
 
 ## kubectl
 apt-get update \
-	&& apt-get install -y apt-transport-https \
-	&& curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - \
-	&& echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
-	&& apt-get update \
-	&& apt-get install -y kubectl
+&& apt-get install -y apt-transport-https \
+&& curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - \
+&& echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
+&& apt-get update \
+&& apt-get install -y kubectl
